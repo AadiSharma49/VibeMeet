967dcec2e83567e33b707b01378ace6654b3fa50
AadiSharma49 <aadi198555@gmail.com>
2026-01-18 13:17:51 +0530

    inital commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { ENV } from './config/env.js';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello from Vibe-Meet backend");
+}
+);
+
+console.log(ENV.MONGO_URI);
+
+app.listen(ENV.PORT,()=>{
+    console.log("Server is running on port "+ENV.PORT);
+});

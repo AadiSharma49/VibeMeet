eae56c766dba8df47db7f2e9702bb51abcc94b9f
AadiSharma49 <aadi198555@gmail.com>
2026-01-23 01:15:00 +0530

    user model fix

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db";
-import User from "../models/User.js";
+import User from "../models/user.model.js";
 import { deleteStreamUser } from "../utils/stream.js";
 
 // Create a client to send and receive events

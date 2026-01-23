418fd6287dbcde8dddf7a93f8fe3efdc2707697f
AadiSharma49 <aadi198555@gmail.com>
2026-01-23 15:03:54 +0530

    fix inngest

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -2,6 +2,7 @@ import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
 
+
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "Vibe-Meet" });
 
@@ -30,7 +31,6 @@ const deleteUser=inngest.createFunction(
         await connectDB();  
         const {id}=event.data;
         await User.deleteOne({clerkId:id});
-        await deleteStreamUser(id.toString());
     }
 )
 

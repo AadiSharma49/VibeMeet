8143cc9b3865c4e9e731feeaf7fb4701900a366f
AadiSharma49 <aadi198555@gmail.com>
2026-03-19 10:39:51 +0530

    Auth provider setup

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,12 @@
+//https://vibe-backend-lake.vercel.app/api
+
+import axios from 'axios';
+
+const BASE_URL=import.meta.env.MODE=="development"?"http://localhost:5000/api":"https://vibe-backend-lake.vercel.app/api";
+
+export const axiosInstance=axios.create(
+    {
+        baseURL:BASE_URL,
+        withCredentials:true,
+    }
+)
